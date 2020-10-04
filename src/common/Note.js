bf1269cfc80243f451775361783e5058df153450
asyaasha <asya.asha@gmail.com>
2020-10-04 10:32:53 -0700

    minor

--- src/common/Note.js
+++ src/common/Note.js
@@ -4,20 +4,20 @@
 
 import React, { useState, useEffect } from "react";
 import moment from "moment";
-import { Spin, Typography, Popconfirm, message } from "antd";
+import { Checkbox, Spin, Typography, Popconfirm, message } from "antd";
 import { SubH2, SpaceBetween, Note1Grey } from "common";
 import { iconTrash } from "media/svg";
 
 const { Paragraph } = Typography;
 
-const Note = ({ authorName, note, deleteNote }) => {
+const Note = ({ authorName, note, deleteNote, extra }) => {
   const [noteText, setNoteText] = useState(note.text);
   const [isSpinning, toggleSpinning] = useState(false);
 
   // rerender text when note updated
-  // useEffect(() => {
-  //   setNoteText(note.text);
-  // }, [note]);
+  useEffect(() => {
+    setNoteText(note.text);
+  }, [note]);
 
   function confirm(e) {
     // TODO delete note
@@ -50,29 +50,38 @@ const Note = ({ authorName, note, deleteNote }) => {
         <span>{moment(note.createdAt).format("MM/D/YYYY hh:mm")} by </span>
         <span style={{ color: "#115CE5" }}>{authorName}</span>
       </Note1Grey>
-      <Paragraph editable={{ onChange: setNoteText }}> {noteText}</Paragraph>
+      <Paragraph {...paragraphProps} editable={{ onChange: setNoteText }}>
+        {" "}
+        {noteText}
+      </Paragraph>
     </div>
   );
+  const renderDelete = deleteNote ? (
+    <Popconfirm
+      title="Are you sure?"
+      onConfirm={confirm}
+      onCancel={cancel}
+      okText="Ok"
+      cancelText="Cancel"
+    >
+      <img
+        onClick={() => toggleSpinning(true)}
+        style={{ cursor: "pointer", marginLeft: 5 }}
+        src={iconTrash}
+        alt="note trash icon"
+      />
+    </Popconfirm>
+  ) : null;
 
   return (
     <div>
       <Spin spinning={isSpinning}>
         <SpaceBetween>
           <SubH2>{note.title}</SubH2>
-          <Popconfirm
-            title="Are you sure?"
-            onConfirm={confirm}
-            onCancel={cancel}
-            okText="Ok"
-            cancelText="Cancel"
-          >
-            <img
-              onClick={() => toggleSpinning(true)}
-              style={{ cursor: "pointer" }}
-              src={iconTrash}
-              alt="note trash icon"
-            />
-          </Popconfirm>
+          <div>
+            <Checkbox />
+            {renderDelete}
+          </div>
         </SpaceBetween>
         {Section}
       </Spin>
