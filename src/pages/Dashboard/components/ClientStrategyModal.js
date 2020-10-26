38a48fd953c9c994aa618718b3ce686eb4da5cee
Asiya <asya.asha@gmail.com>
2020-10-26 00:48:49 -0700

    fix strtaegy fetch

--- src/pages/Dashboard/components/ClientStrategyModal.js
+++ src/pages/Dashboard/components/ClientStrategyModal.js
@@ -1,38 +1,40 @@
 import React from "react";
 import gql from "graphql-tag";
 import { useQuery } from "@apollo/react-hooks";
-import { listClientStrategys } from "graphql/queries";
+import { getClient } from "graphql/queries";
 import { Modal, Divider } from "antd";
-import { Note } from "common";
+import { Note, Loading } from "common";
 
 const ClientStrategyModal = ({
   selectedClientId,
   handleToggle,
   isBadgeModal,
 }) => {
-  const { loading, data, error } = useQuery(gql(listClientStrategys), {
-    filter: {
-      clientId: selectedClientId,
-    },
+  const { loading, data, error } = useQuery(gql(getClient), {
+    variables: { id: selectedClientId },
   });
 
   const isLoading = loading || error;
-  const clientStrategys = !isLoading ? data.listStrategys.items : [];
+  const clientStrategys = !isLoading ? data.getClient.strategy.items : [];
 
-  const renderNotes = clientStrategys.map((item) => {
-    return (
-      <div style={{ marginTop: 15 }}>
-        <Note
-          height={95}
-          type="strategy"
-          authorName={item.ownerId.name}
-          note={item}
-          deleteNote={() => console.log("delete")}
-        />
-        <Divider />
-      </div>
-    );
-  });
+  const renderNotes = loading ? (
+    <Loading />
+  ) : (
+    clientStrategys.map((item) => {
+      return (
+        <div style={{ marginTop: 15 }}>
+          <Note
+            height={95}
+            type="strategy"
+            authorName={data.getClient.name}
+            note={item}
+            deleteNote={() => console.log("delete")}
+          />
+          <Divider />
+        </div>
+      );
+    })
+  );
 
   return (
     <Modal
