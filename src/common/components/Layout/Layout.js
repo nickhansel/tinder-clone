5c25c7a5eaa4cd48a2d5c102477f7f19adfbabd6
Asiya <asya.asha@gmail.com>
2020-10-26 12:56:45 -0700

    update layout

--- src/common/components/Layout/Layout.js
+++ src/common/components/Layout/Layout.js
@@ -2,7 +2,7 @@
   Layout component
  */
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 // import JSignOut from "../../../auth/SignOut";
 import { SearchInput, SubH1, Profile, Flex } from "common";
@@ -11,7 +11,6 @@ import {
   IconStyled,
   HeaderStyled,
   HeaderActions,
-  StyledSider,
   StyledTopHeader,
 } from "./styles";
 import { BASE_URLS } from "utils";
@@ -20,9 +19,18 @@ import "./styles.css";
 
 import { Layout as AntLayout, Menu } from "antd";
 
+const { Sider } = AntLayout;
+const { Item } = Menu;
+
 const Layout = ({ prefix, children, title, extra = null }) => {
   const [collapsed, setCollapsed] = useState(true);
+  const [selectedItem, setSelectedItem] = useState("dashboard")
   let history = useHistory();
+
+  useEffect(()=> {
+    const {pathname} = history.location;
+    setSelectedItem(pathname.substring(1))
+  }, [history])
 
   const toggle = () => {
     setCollapsed(!collapsed);
@@ -34,37 +42,47 @@ const Layout = ({ prefix, children, title, extra = null }) => {
   const renderIconGear = <IconStyled src={iconGear} alt="logo settings" />;
   const renderIconPulse = (
     <IconStyled
-      onClick={() => handleMenuItemClick(BASE_URLS.INSIGHTS)} // TODO uncomment when the views are ready
       src={iconPulse}
       alt="logo insights"
     />
   );
   const renderIconPerson = (
     <IconStyled
-      onClick={() => handleMenuItemClick(BASE_URLS.DASHBOARD)}
       src={iconPerson}
       alt="logo dashboard"
     />
   );
-
+    console.log(selectedItem)
   return (
     <AntLayout>
-      <StyledSider trigger={null} collapsible collapsed={collapsed}>
+      <Sider trigger={null} collapsible collapsed={collapsed}>
         <div className="logo-side-bar">
           <img src={iconLogo} onClick={() => toggle()} alt="logo side bar" />
         </div>
-        <Menu defaultSelectedKeys={["1"]}>
-          <Menu.Item key="1" icon={renderIconPerson}>
+        <Menu
+          theme="dark" 
+          mode="inline"
+          selectable={true}
+          onClick={(item) => handleMenuItemClick(item.key)}
+          onSelect={(key) => setSelectedItem(key)}
+          inlineCollapsed={true}
+          selectedKeys={[selectedItem]}
+          defaultSelectedKeys={["dashboard"]}>
+          <Item
+            key="dashboard"
+            icon={renderIconPerson}>
             Dashboard
-          </Menu.Item>
-          <Menu.Item key="2" icon={renderIconPulse}>
+          </Item>
+          <Item
+            key="insights"
+            icon={renderIconPulse}>
             Insights
-          </Menu.Item>
-          <Menu.Item key="3" icon={renderIconGear}>
+          </Item>
+          <Item key="settings" icon={renderIconGear}>
             Settings
-          </Menu.Item>
+          </Item>
         </Menu>
-      </StyledSider>
+      </Sider>
       <AntLayout className="site-layout">
         <StyledTopHeader>
           {renderIconPerson}
